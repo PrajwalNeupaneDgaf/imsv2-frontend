cab0fd5c5f23da2909c5a473ca16e82bbb5db02d
PrajwalNeupaneDgaf <157794663+PrajwalNeupaneDgaf@users.noreply.github.com>
2024-12-03 02:06:36 +0545

    Your commit message here

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 // Create Axios instance
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://ims-backend-jsj5.onrender.com/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
